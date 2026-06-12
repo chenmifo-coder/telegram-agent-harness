ee4945867be0516cfade0cb1e2fa2762900fe05c
chenmifo-coder <chenmifo@gmail.com>
2026-06-12 14:58:42 +0800

    Update telegram_utils.py

--- telegram_utils.py
+++ telegram_utils.py
@@ -1,10 +1,9 @@
 import os
 import requests
 
-TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
-
 def send_message(chat_id, text):
-    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    token = os.environ["TELEGRAM_TOKEN"]
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
     data = {"chat_id": chat_id, "text": text}
     requests.post(url, json=data)
 
