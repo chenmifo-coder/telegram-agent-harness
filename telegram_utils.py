d1b0975f950b7fbe6ab8ef7258d34777503d1108
chenmifo-coder <chenmifo@gmail.com>
2026-06-16 15:31:43 +0800

    Update telegram_utils.py

--- telegram_utils.py
+++ telegram_utils.py
@@ -1,13 +1,13 @@
 import os
 import requests
-from config import TELEGRAM_TOKEN
 
-def send_message(chat_id: int, text: str):
-    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+def send_message(chat_id, text):
+    token = os.environ["TELEGRAM_TOKEN"]
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
     data = {"chat_id": chat_id, "text": text}
     requests.post(url, json=data)
 
-def set_webhook(token: str, webhook_url: str):
+def set_webhook(token, webhook_url):
     url = f"https://api.telegram.org/bot{token}/setWebhook"
     data = {"url": webhook_url}
     resp = requests.post(url, json=data)
